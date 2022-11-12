92719e3ecf11b1cce005322eb24007201e6d40b8
Samuli Öhman <samuli.ohman@aalto.fi>
2022-11-12 23:33:57 +0200

    fix: Added comments for the functions.

--- src/Coordinate.hpp
+++ src/Coordinate.hpp
@@ -5,16 +5,20 @@
 
 class Coordinate {
  public:
+  // Constructor takes which takes x and y as parameters.
   Coordinate(int x, int y) : x_(x), y_(y) {}
 
+  // Overloading the + operator for adding multiple Coordinates together.
   Coordinate operator+(const Coordinate& c2) {
     return Coordinate(this->x_ + c2.x_, this->y_ + c2.y_);
   }
 
+  // Overloading the - operator for subtracting multiple Coordinates together.
   Coordinate operator-(const Coordinate& c2) {
     return Coordinate(this->x_ - c2.x_, this->y_ - c2.y_);
   }
 
+  // Overloading << method for printing vector in ostreams.
   friend std::ostream& operator<<(std::ostream& os, Coordinate& c) {
     os << "(" << c.x_ << "," << c.y_ << ")";
     return os;
--- src/Map.hpp
+++ src/Map.hpp
@@ -9,18 +9,28 @@ class Map {
  public:
   Map() : nOfNodes_(0) {}
 
+  // Adds a single Coordinate to the enemies "path".
   void AddCoordinate(const Coordinate& coord);
 
+  // Adds a vector Coordinates to the enemies "path".
   void AddCoordinates(std::vector<Coordinate> coordinates);
 
+  // Gets the path of the enemies.
   std::vector<Coordinate> GetNodes() const;
 
+  // Gets a single node from the path.
+  // Takes the index of the node as a parameter.
   Coordinate GetNode(const int& index) const;
 
+  // Gets the amount of nodes.
+  // If enemy is on the last node, it means enemy got through the defenses.
   int GetNofNodes() const;
 
  private:
+  // Enemies path
   std::vector<Coordinate> nodes_;
+
+  // amount of nodes
   int nOfNodes_;
 };
 
