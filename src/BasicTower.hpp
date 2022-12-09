2f6c96d8d3d15063ca9d258c758137268c4f4974
Samuli Öhman <samuli.ohman@aalto.fi>
2022-12-09 15:58:12 +0200

    Fix: fixed different towers to shoot correctly.

--- src/BasicTower.hpp
+++ src/BasicTower.hpp
@@ -5,35 +5,10 @@
 
 class BasicTower : public Tower {
  public:
-  BasicTower(Coordinate place, Game* game) : Tower(150, 3, 1, place, 2, game){};
+  BasicTower(Coordinate place, Game* game)
+      : Tower(200, 5, 1, place, 150, game, 0){};
 
-  void Defend() {
-    Game* game = this->GetGame();
-    std::vector<Enemy*> e = game->GetEnemies();
-    std::vector<Enemy*> inRange;
-
-    for (auto i : e) {
-      auto a = i->GetCoord();
-      auto b = this->GetPlace();
-      auto dist = (a - b).getLength();
-      // auto distance = ((i->GetCoord()) - place_).getLength();
-      // std::cout << dist;
-      if (dist < this->GetRange()) inRange.push_back(i);
-    }
-    std::sort(inRange.begin(), inRange.end());
-
-    if (!inRange.empty()) {
-      auto enemy = inRange.front();
-      auto a = enemy->GetCoord();
-      auto b = this->GetPlace();
-      auto dist = (a - b).getLength();
-      auto dir = (a - b) / dist;
-      auto proj =
-          Projectile(3, this->GetDamage(), this->GetPlace(), dir, normal, game);
-
-      game->AddProjectile(proj);
-    }
-  };
+  void Defend() { this->Shoot(normal); }
 };
 
 #endif
